74964fccb5c75848554e9e169e9eaa1f3c96ba6f
Kevni92 <mail@kevinkrone.de>
2026-07-19 22:34:31 +0200

    Configure Quest WebXR store

---
+++ src/components/VR/xrStore.ts
@@ -0,0 +1,14 @@
+import { createXRStore } from "@react-three/xr";
+
+export const xrStore = createXRStore({
+  offerSession: false,
+  enterGrantedSession: false,
+  emulate: false,
+  hand: false,
+  gaze: false,
+  screenInput: false,
+  frameRate: "high",
+  foveation: 0.8,
+  frameBufferScaling: 0.8,
+  originReferenceSpace: "local-floor",
+});
