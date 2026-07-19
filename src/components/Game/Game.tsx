3e84984e1058125df9ca63935ed20861e30bcca0
Kevni92 <mail@kevinkrone.de>
2026-07-19 22:39:22 +0200

    Switch between desktop and Quest VR modes

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,23 +1,33 @@
+"use client";
+
+import { memo, useState } from "react";
+
 import Navigation from "../Navigation/Navigation";
-import SettingsMenu from "../HUD/SettingsMenu/SettingsMenu";
-import { memo } from "react";
-import "./Game.scss";
 import Scene from "../Scene/Scene";
 import HUD from "../HUD/HUD";
 import DevTools from "../DevTools";
+import VRExperience from "../VR/VRExperience";
+import "./Game.scss";
 
 import { AsteroidRuntimeProvider } from "@/sim/asteroids/runtimeContext";
 import { WorldOriginProvider } from "@/sim/worldOrigin";
 
 const Game = () => {
+  const [vrActive, setVrActive] = useState(false);
+
   return (
     <WorldOriginProvider>
       <AsteroidRuntimeProvider>
         <div className="container">
-          <Scene />
-          <HUD />
-          <Navigation />
-          <DevTools />
+          {!vrActive ? (
+            <>
+              <Scene />
+              <HUD />
+              <Navigation />
+              <DevTools />
+            </>
+          ) : null}
+          <VRExperience active={vrActive} onActiveChange={setVrActive} />
         </div>
       </AsteroidRuntimeProvider>
     </WorldOriginProvider>
